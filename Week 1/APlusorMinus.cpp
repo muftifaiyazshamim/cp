cbe2abfa4c723c3852d99c59a1128ae7adff6009
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-08-24 18:24:21 +0600

    update

--- Week 1/APlusorMinus.cpp
+++ Week 1/APlusorMinus.cpp
@@ -1,7 +1,15 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
+    int t;cin>>t;
+    while (t>0)
+    {int a,b,c;cin>>a>>b>>c;
+        if (a+b==c)cout<<'+'<<endl;
+        else if(a-b==c)cout<<'-'<<endl;
+
+        /* code */
+    t--;}
     
     
 
