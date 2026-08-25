11643c26a68c2c2ee99ffa75bf3c0ef2c727be1e
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-08-25 22:54:26 +0600

    update

--- AWrongSubtraction.cpp
+++ AWrongSubtraction.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    int a,b;cin>>a>>b;
-    for (int i = 0; i < b; i++)
+    long long int a,b;cin>>a>>b;
+    for (long long int i = 0; i < b; i++)
     {
         if(a%10==0) a=a/10;
         else a=a-1;
