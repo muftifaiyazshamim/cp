9501937110755675f93ace0028466b160f837145
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-08-26 17:09:09 +0600

    update

--- ODDEVEN7.cpp
+++ ODDEVEN7.cpp
@@ -4,15 +4,18 @@ using namespace std;
 int main() {
 	int t;cin>>t;
 	for(int i=0;i<t;i++){
-	    int a,o=0,e=0;
-	    vector<int>v ;
+	    long long int a,o=0,e=0;
+        cin>>a;
+	    vector<long long int>v ;
 	    for(int j=0;j<a;j++){
-	        int x;cin>>x;v.push_back(x);
+	        long long int x;cin>>x;v.push_back(x);
             if (x%2==0) e++;
             else o++;
             
 	    }
-        
+        if(e==o) cout<<o+e<<endl;
+        else if(e>o) cout<<o+2<<endl;
+        else  cout<<e+2<<endl;
 	    
 	    
 	}
