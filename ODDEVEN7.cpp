e8b826661fe686a759117297f2b3027b0722a703
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-08-26 17:03:55 +0600

    update

---
+++ ODDEVEN7.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;cin>>t;
+	for(int i=0;i<t;i++){
+	    int a,o=0,e=0;
+	    vector<int>v ;
+	    for(int j=0;j<a;j++){
+	        int x;cin>>x;v.push_back(x);
+            if (x%2==0) e++;
+            else o++;
+            
+	    }
+        
+	    
+	    
+	}
+
+}
