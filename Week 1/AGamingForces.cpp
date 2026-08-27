3890b4eb8d684161452b71e9e24809fa4d9ab0ee
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-08-28 02:21:14 +0600

    update

--- Week 1/AGamingForces.cpp
+++ Week 1/AGamingForces.cpp
@@ -1,24 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-//vbal osiena
+
 int main() {
     int t;cin>>t;
     while(t>0){
-    int a;cin>>a;
+    int a,o=0,r=0;cin>>a;
     vector <int> v;
     for (int i = 0; i <a; i++)
     {
         int x;cin>>x;v.push_back(x);
+        if (x==1) o++;
+        if (x>=2) r++;
+        
     }
-    int c=0;
-   for (int i = 0; i < a; i++)
-   {
-    if ((v[i]==1 && v[i+1]==1)||(v[i]==0 && v[i+1]==1)||(v[i]==1 && v[i+1]==0)) {c++; if(v[i]!=0) v[i]=v[i]-1; else if(v[i+1]!=0) v[i+1]=v[i+1]-1;}
-    else if(v[i]>=2) c++;
-    
-   }
-   cout <<c<<endl;
-
-    }
+    int to=o/2;
+    if (o%2==1) to=to+1;
+    cout<<to+r<<endl;
+    t--;}
     return 0;
 }
