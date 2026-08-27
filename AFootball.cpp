5552dacf853c4478357435b5ec1abd0cf50d9cca
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-08-27 23:16:03 +0600

    update

--- AFootball.cpp
+++ AFootball.cpp
@@ -4,19 +4,16 @@ using namespace std;
 int main() {
     string s;cin>>s;
     int z=0,o=0;
+    
     for (int i = 0; i < s.size(); i++)
     {
-        if(s[i]=='1') o++;
-        else if(s[i]=='0') continue;
-        else break;
+        string su=s.substr(i,7);
+        if(su=="1111111") o++;
+        // else if(s[i]=='0') continue;
+        else if(su=="0000000") z++;
+
     }
-     for (int i = 0; i < s.size(); i++)
-    {
-        if(s[i]=='0') z++;
-        else if(s[i]=='1') continue;
-        else break;
-        
-    }
+     
     if(o>=7 || z>=7) cout<<"YES"<<endl;
     else cout<<"NO"<<endl;
     return 0;
