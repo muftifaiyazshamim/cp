89cc067ca5fb276a6169808de17cc9dd6aa0b04b
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-01 20:33:25 +0600

    update

--- ASerejaandDima.cpp
+++ ASerejaandDima.cpp
@@ -4,40 +4,38 @@ using namespace std;
 int main() {
     
     int a;
-    cin>>a;vector<int>v;
+    cin>>a;
+    deque<int>v;
     for (int i = 0; i < a; i++)
     {
         int x;cin>>x;v.push_back(x);
     }
     int s=0;
     int d=0;
-    if(a%2==0){
-    for (int i = 0,j=a-1; i < a/2;j--, i++)
+    while(!v.empty())
     {
-        int mx1=max(v[i],v[j]);
-        s=s+mx1;
-        if(mx1==v[j]) d=d+max(v[i],v[j-1]);
-        else if(mx1==v[i]) d=d+max(v[i-1],v[j]);
-    }
-    }
-    else{
-    for (int i = 0,j=a-1; i < a/2-1;)
-    {
-        int mx1=max(v[i],v[j]);
-        if(mx1==v[j]) {
-            s=s+max(v[i],v[j]);
-            d=d+max(v[i],v[j-1]);
-            i++;
-        }
-        else if(mx1==v[i]) {
-            s=s+max(v[i],v[j]);
-            d=d+max(v[i-1],v[j]);
-            j--;
+        int mx=max(v.front(),v.back());
+        if(mx==v.front()){
+            s=s+mx;
+            v.pop_front();
+            if(!v.empty()){
+             mx=max(v.front(),v.back());
+             d=d+mx;
+             if(mx==v.front()) v.pop_front();
+             else if (mx==v.back()) v.pop_back();
+            }
             
         }
-    }
-
-
+        else if(mx==v.back()){
+            s=s+mx;
+            v.pop_back();
+            if(!v.empty()){
+             mx=max(v.front(),v.back());
+             d=d+mx;
+             if(mx==v.front()) v.pop_front();
+             else if (mx==v.back()) v.pop_back();
+            }
+        }
     }
     cout<<s<<" "<<d;
     return 0;
