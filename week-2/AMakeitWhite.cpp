29ac6998d9ee706536f3886774368bd55d8c4e7c
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-01 12:09:24 +0600

    update

---
+++ week-2/AMakeitWhite.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;cin>>t;
+    while (t>0)
+    {int a;cin>>a;
+     string s,s2;cin>>s;
+        int fp=0,lp=0;
+        fp=s.find('B');
+        lp=s.rfind('B');
+
+        cout<<fp-lp+1<<endl;   
+    t--;}
+    
+    
+    return 0;
+}
