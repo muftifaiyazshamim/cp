3cceb77cdfb311af50e9f92e727953bd58681daa
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-03 23:29:21 +0600

    update

--- Week 1/MAXSUM77.cpp
+++ Week 1/MAXSUM77.cpp
@@ -6,38 +6,24 @@ int main() {
 	 for (int i = 0; i <t; i++){
 	     int a,b,s=0;cin>>a>>b;
 	    
-	     deque<int>d ;
+	     deque<int>d,p ;
 	     for (int j = 0; j <a; j++){
 	     int x;cin>>x;s=s+x;
 	    
          d.push_back(x);
 	     }
-         for (int j = 0; j <b; j++){
-            
-            {
-                if(min(s-d.front(),s-d.back())==s-d.front()){
-                    if(j==b-1) break;
-                    
-                    s=s-d.front();
-                    d.pop_front();
-                    
-                } 
-                else if(min(s-d.front(),s-d.back())==s-d.back()){
-                    if(j==b-1) break;
-                    
-                    s=s-d.back();
-                    d.pop_back();
+         for (int j = 0; j <=b; j++){
+                int mx=0;
+                for (int k = j; k <j+a-b; k++){
+                mx=mx+d[k];
+
                 }
-            }
-            
+                p.push_back(mx); 
+         }    
+        std::sort(p.rbegin(), p.rend());    
+        cout<<p[0]<<endl;    
          
-         int su=0;
-         for(auto y:d){
-             su=su+y;
-         }
-         cout<<su<<endl;
-	     
-        }
+        
   
 }
 return 0;  
