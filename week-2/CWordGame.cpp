22c87d1e5870cbfa7d1af1712c96ce28b5df23df
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-06 23:12:40 +0600

    update

--- week-2/CWordGame.cpp
+++ week-2/CWordGame.cpp
@@ -19,28 +19,31 @@ int main() {
     {
         string x;cin>>x;v3.push_back(x);
     }int f1=0,f2=0,f3=0;
-    for (int i = 0; i <t ; i++)
+    sort(v1.begin(),v1.end());
+    sort(v2.begin(),v2.end());
+    sort(v3.begin(),v3.end());
+    for (int i = 0; i <v1.size() ; i++)
     {
-    auto it1 = find(v2.begin(), v2.end(), v1[i]);
-    auto it2 = find(v3.begin(), v3.end(), v1[i]);
-    if(it1!=v2.end() && it2!=v3.end()) f1=f1+0;
-    else if(it1==v2.end() && it2!=v3.end()||(it1!=v2.end() && it2==v3.end()))  f1=f1+1;   
+    bool it1 = binary_search(v2.begin(), v2.end(), v1[i]);
+    bool it2 =  binary_search(v3.begin(), v3.end(), v1[i]);
+    if(it1 && it2) f1=f1+0;
+    else if(it1||it2)  f1=f1+1;   
     else f1=f1+3;
     }
-     for (int i = 0; i <t ; i++)
+     for (int i = 0; i <v2.size() ; i++)
     {
-    auto it1 = find(v1.begin(), v1.end(), v2[i]);
-    auto it2 = find(v3.begin(), v3.end(), v2[i]);
-    if(it1!=v1.end() && it2!=v3.end()) f2=f2+0;
-    else if(it1==v1.end() && it2!=v3.end()||(it1!=v1.end() && it2==v3.end()))  f2=f2+1;   
+    bool it1 =  binary_search(v1.begin(), v1.end(), v2[i]);
+    bool it2 =  binary_search(v3.begin(), v3.end(), v2[i]);
+    if(it1 && it2) f2=f2+0;
+    else if(it1||it2)  f2=f2+1;   
     else f2=f2+3;
     }
-     for (int i = 0; i <t ; i++)
+     for (int i = 0; i <v3.size() ; i++)
     {
-    auto it1 = find(v1.begin(), v1.end(), v3[i]);
-    auto it2 = find(v2.begin(), v2.end(), v3[i]);
-    if(it1!=v1.end() && it2!=v2.end()) f3=f3+0;
-    else if(it1==v1.end() && it2!=v2.end()||(it1!=v1.end() && it2==v2.end()))  f3=f3+1;   
+    bool it1 = binary_search(v1.begin(), v1.end(), v3[i]);
+    bool it2 =  binary_search(v2.begin(), v2.end(), v3[i]);
+    if(it1 && it2) f3=f3+0;
+    else if(it1||it2)  f3=f3+1;   
     else f3=f3+3;
     }
     cout<<f1<<" "<<f2<<" "<<f3<<endl;
