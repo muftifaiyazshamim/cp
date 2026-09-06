48a8415a95cb1361c3ae78b9d6221b7c22ac5f28
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-06 23:00:24 +0600

    update

--- BMinusTwo.cpp
+++ BMinusTwo.cpp
@@ -3,6 +3,25 @@ using namespace std;
 
 int main() {
     
+    int t;cin>>t;
+    while (t>0)
+    {
+        int a;cin>>a;
+        vector<int>v;
+        int o=0,e4=0,e=0;
+        for (int i = 0; i < a; i++)
+        {
+            int x;cin>>x;v.push_back(x);
+            if(x%2==1) o++;
+            else{
+                if(x%4==0) e4++;
+                else e++;
+            }
+        }
+        cout<<max(o,max(e,e4))<<endl;
+
+        /* code */
+    t--;}
     
 
     return 0;
