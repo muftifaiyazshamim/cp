2719c45ba577f4f1b28848c23ba6d6fea2dc3b39
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-06 21:20:58 +0600

    update

---
+++ week-2/CWordGame.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;cin>>t;
+    vector<int> v1,v2,v3;
+    for (int i = 0; i < t; i++)
+    {
+        int x;cin>>x;v1.push_back(x);
+    }
+    for (int i = 0; i < t; i++)
+    {
+        int x;cin>>x;v2.push_back(x);
+    }
+    for (int i = 0; i < t; i++)
+    {
+        int x;cin>>x;v3.push_back(x);
+    }
+    
+
+    return 0;
+}
