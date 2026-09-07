f28f993101e2808b57d4ce3a457c44834d88e011
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-07 21:24:51 +0600

    update

---
+++ week-2/CRegistrationsystem.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;cin>>t;
+    vector<string>v;
+    while (t>0)
+    
+    {string s;cin>>s;
+   v.push_back(s);
+   
+    t--;}
+    
+    
+
+    return 0;
+}
