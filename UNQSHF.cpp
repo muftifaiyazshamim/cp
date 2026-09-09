d3effe4803a615a0e6fe97f57810b7db1af1c4c4
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-09 20:41:11 +0600

    update

---
+++ UNQSHF.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;cin>>t;
+	while(t>0){
+	    int al;string s,s2;cin>>al;
+	    cin>>s;
+	    cin>>s2;
+	    if(al%2==1) {
+	        
+	        }
+	   else{
+	       int a,b;
+	       for (int i = 0; i < al; i++)
+           {
+            if(s[i]=='a') a++;
+            else b++;
+           }
+           
+	   }     
+	t--;}
+}
