9e0c1c959341d889fa47a6846eafd1efa9d61913
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-09 21:45:54 +0600

    update

--- BTCMINE.cpp
+++ BTCMINE.cpp
@@ -5,12 +5,13 @@ int main() {
     
     int t;cin>>t;while (t>0)
     {int a,b,k=1,earn=0;cin>>a>>b;
-        earn=b*k*k;//day 1
+        earn=b*k*k;//day 1 earning
+        int cost=-(b*k*k);
         for (int i = 2;; i++)
         {
             earn=earn+(b*k*k);
             if (earn>=a){ earn=earn-a;k++;} 
-            
+            cost=cost-earn;
            
         }
         
