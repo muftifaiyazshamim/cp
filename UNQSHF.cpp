82c4988ddbb0c884732071348e536c5e1a8d4fd8
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-09 21:08:02 +0600

    update

--- UNQSHF.cpp
+++ UNQSHF.cpp
@@ -8,13 +8,13 @@ int main() {
 	    cin>>s;
 	    cin>>s2;
         int a1,b1,a2,b2;
-	       for (int i = 0; i < al; i++)
-           {
+	    for (int i = 0; i < al; i++)
+        {
             if(s[i]=='a') a1++;
             else if(s[i]=='b') b1++;
             else if(s2[i]=='a') a2++;
             else if(s2[i]=='a') b2++;
-           }
+        }
 	    if(al%2==1){
 	        
 	        }
