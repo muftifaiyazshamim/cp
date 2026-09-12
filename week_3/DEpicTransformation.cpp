2b3fad325273eb686e97299f3a0dfa92b0db6696
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-12 20:49:16 +0600

    update

---
+++ week_3/DEpicTransformation.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;cin>>t;while (t>0)
+    {int a,c=0;cin>>a;vector<int>v;
+    for (int i = 0; i < a; i++)
+    {
+        int x;cin>>x;v.push_back(x);
+    }
+        
+        
+   t--;}
+    
+    
+
+    return 0;
+}
