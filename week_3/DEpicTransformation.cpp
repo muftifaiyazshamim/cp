20da3e95796417bfe59723c87c4841f30a1b3c81
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-14 21:26:35 +0600

    update

--- week_3/DEpicTransformation.cpp
+++ week_3/DEpicTransformation.cpp
@@ -3,13 +3,19 @@ using namespace std;
 
 int main() {
     int t;cin>>t;while (t>0)
-    {int a,c=0;cin>>a;vector<int>v;
+    {int a,c=0;cin>>a;vector<int>v;map<int,int>mp;
+        vector<pair<int,int>>v2;
     for (int i = 0; i < a; i++)
     {
         int x;cin>>x;v.push_back(x);
+        mp[x]++;      
     }
-        
-        
+    for (auto [x,y]:mp )
+    {
+        cout<<y<<"<-"<<x<<endl;
+    }
+    
+     
    t--;}
     
     
