25b50f10314c201d1abba6ab31070ea9574dc5e4
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-16 21:34:14 +0600

    update

--- BUSSEAT.cpp
+++ BUSSEAT.cpp
@@ -3,11 +3,21 @@ using namespace std;
 
 int main() {
 int t;cin>>t;while(t>0){
-    int a,b;cin>>a>>b;
+    int a,b,c=1,f=0;cin>>a>>b;
     int arr[a][2];
-
+for (int i = 0; i < a; i++)
+{
+    for (int j = 0; j < 2; i++)
+    {
+        arr[i][j]=c;
+        c++;
+        if(c>a) f++;
+    }
+    
+}
+cout<<f*2<<endl;
     
     t--;
 }
 
-rertun 0;}
+return 0;}
