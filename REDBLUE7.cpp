74bd94f416a40da0b547e459bacace43ebd7d764
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-17 23:42:24 +0600

    update

--- REDBLUE7.cpp
+++ REDBLUE7.cpp
@@ -4,20 +4,27 @@ using namespace std;
 int main() {
 int t;cin>>t;while(t>0){
     long long int a;cin>>a;
-    std::vector<long long int> v;
-   for (int i = 0; i < a; i++){
-       long long int c;cin>>c;v.push_back(c);
+    std::vector<long long int> v(a+1);
+   for (int i = 1; i <= a; i++){
+       cin>>v[i];
    }
-    std::sort(v.begin(), v.end());
-    long long int m=v.size()/2;
-    long long int p1=0,p2=0,p1c=0,p2c=0;
-    for (int i = 0; i <= m; i++){
-        p1=v[i]+p1;p1c++;
+    sort(v.begin()+1, v.end());
+    vector<long long int>v2(a+1);
+    v2[1]=v[1];
+    for (int i = 2; i <= a; i++){
+       v2[i]=v[i]+v2[i-1];
+   }
+   long long int ans=0;
+    for (int i = 1; i <= a; i++){
+        long long int su=v2[i]-v2[0];
+        long long int bu=v2[a]-v2[i];
+        long long int cu=i;
+        long long int lu=a-i;
+        long long int cur=su*lu+bu*cu;
+        ans=max(ans,cur);
     }
-    for (int i = m+1; i < v.size(); i++){
-        p2=p2+v[i];p2c++;
-    }
-    cout<<p1*p2c+p2*p1c<<endl;
+    cout<<ans<<endl;
+    
 t--;}
 
 }
