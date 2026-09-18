82a200fd56dd10bc652b3bac800a89e6dd310bef
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-18 14:34:05 +0600

    update

--- week_3/CNumericStringTemplate.cpp
+++ week_3/CNumericStringTemplate.cpp
@@ -5,8 +5,6 @@ int main() {
     int t;cin>>t;
     while (t>0)
     {
-        
-
         int a,b;cin>>a;
         vector<int>v1;
         for (int i = 0; i < a; i++)
@@ -23,17 +21,16 @@ int main() {
         vector<string>ans;
         for (int i = 0; i < v2.size(); i++)
         { 
-             map<char,int>mp;
-             map<int,char>mp2;
+            map<char,int>mp;
+            map<int,char>mp2;
             string s=v2[i];int cu=0;
             if(s.size()!=v1.size())           
             {
                 ans.push_back("NO");
                 continue;
             }
-              for (int j = 0; j <s.size(); j++)
+            for (int j = 0; j <s.size(); j++)
             {
-                
                 if(mp.count(s[j])==0 && mp2.count(v1[j])==0)
                 {
                     mp[s[j]]=v1[j];
