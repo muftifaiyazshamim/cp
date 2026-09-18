621df3e5d6f18117f79e9f33a0ead62476b430d3
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-18 14:33:44 +0600

    update

--- week_3/CNumericStringTemplate.cpp
+++ week_3/CNumericStringTemplate.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main() {
     int t;cin>>t;
-    vector<string>ans;
     while (t>0)
     {
+        
+
         int a,b;cin>>a;
         vector<int>v1;
         for (int i = 0; i < a; i++)
@@ -19,24 +20,32 @@ int main() {
             string al;cin>>al;v2.push_back(al);
               
         }
-        map<char,int>mp;
+        vector<string>ans;
         for (int i = 0; i < v2.size(); i++)
-        {
-            string s=v2[i];int cu;
-            if(s.size()==v1.size())
-            {   
-                for (int j = 0; j < s.size(); j++)
-                {
-                    if(mp[s[j]]==0) mp[s[j]]=v1[j];
-                }
-                for (int j = 0; j < s.size(); j++)
-                {
-                    if(mp[s[j]]==v1[j]) cu++;
-                }
-                if(cu==s.size()) ans.push_back("YES");
-                else ans.push_back("NO");
+        { 
+             map<char,int>mp;
+             map<int,char>mp2;
+            string s=v2[i];int cu=0;
+            if(s.size()!=v1.size())           
+            {
+                ans.push_back("NO");
+                continue;
             }
-
+              for (int j = 0; j <s.size(); j++)
+            {
+                
+                if(mp.count(s[j])==0 && mp2.count(v1[j])==0)
+                {
+                    mp[s[j]]=v1[j];
+                    mp2[v1[j]]=s[j];
+                }
+            }
+            for (int j = 0; j < s.size(); j++)
+            {
+                if(mp.count(s[j]) && mp[s[j]]==v1[j]) cu++;
+            }
+            if(cu==s.size()) ans.push_back("YES");
+            else ans.push_back("NO");
         }
         for(auto x:ans){
             cout<<x<<endl;
