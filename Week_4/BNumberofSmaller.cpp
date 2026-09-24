7d59282453d1581767df7a9b2532f9feca4ffa65
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-24 23:57:40 +0600

    update

--- Week_4/BNumberofSmaller.cpp
+++ Week_4/BNumberofSmaller.cpp
@@ -13,11 +13,14 @@ int main() {
     {
         long long int x;cin>>x;v2.push_back(x);
     }
+    sort(v.begin(), v.end());
+    sort(v2.begin(), v2.end());
+    long long int c=0;
     for (int i = 0; i < b; i++)
-    {long long int c=0;
-        for (long long int j = 0; j < a; j++)
+    {
+        while(v2[i]>v[c])
         {
-            if(v2[i]>v[j]) c++;
+           c++;
         }
         cout<<c<<" ";
     }
