67d6eaca9d7f98b8b0f08e11fe52f1496f20f094
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-25 17:10:15 +0600

    update

---
+++ Week_4/APhoenixandGold.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;cin>>t;
+    while (t>0)
+    {int a,b,s=0;cin>>a>>b;
+        vector<int>v;
+        for (int i = 0; i < a; i++)
+        {
+            int x;cin>>x;v.push_back(x);
+            s=s+x;
+        }
+        if(s==b) cout<<"NO"<<endl;
+        else if(s<b){
+             cout<<"YES"<<endl;
+            for (auto x: v)
+            {
+                cout<<x<<" ";
+            }
+            cout<<endl;
+            }
+        else{
+            sort(v.rbegin(),v.rend());
+            int su=0;
+            for (int i = 0; i < v.size(); i++)
+            {
+                su=su+v[i];
+                if(su==b) swap(v[i],v[i+1]);
+            }
+            for (auto x: v)
+            {
+                cout<<x<<" ";
+            }
+            cout<<endl;
+        }    
+        
+    t--;}
+    
+
+    return 0;
+}
