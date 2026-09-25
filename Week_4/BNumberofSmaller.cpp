06ea3e0bb628490e3c759ba08b8b85f2b148c133
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2026-09-25 17:23:16 +0600

    update

--- Week_4/BNumberofSmaller.cpp
+++ Week_4/BNumberofSmaller.cpp
@@ -13,8 +13,7 @@ int main() {
     {
         long long int x;cin>>x;v2.push_back(x);
     }
-    sort(v.begin(), v.end());
-    sort(v2.begin(), v2.end());
+ 
     long long int c=0;
     for (int i = 0; i < b; i++)
     {
